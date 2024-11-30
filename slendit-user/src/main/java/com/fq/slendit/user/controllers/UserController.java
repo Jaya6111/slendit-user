700a4946e963a8041d177d095f9e7fe3dc8412ef
jaya surya <jaya6111@gmail.com>
2024-11-30 17:42:32 +0530

    adding controller

---
+++ slendit-user/src/main/java/com/fq/slendit/user/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.fq.slendit.user.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@PostMapping("/register")
+	public String save(@RequestParam String s) {
+		return s;
+		
+	}
+}
