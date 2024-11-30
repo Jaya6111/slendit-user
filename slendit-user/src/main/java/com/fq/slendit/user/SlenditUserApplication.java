5704ae973b987947ff339767bd86713ee1c1c4d3
Jaya6111 <136075867+Jaya6111@users.noreply.github.com>
2024-11-30 17:03:36 +0530

    Upload
    creating slendit user application

---
+++ slendit-user/src/main/java/com/fq/slendit/user/SlenditUserApplication.java
@@ -0,0 +1,13 @@
+package com.fq.slendit.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlenditUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlenditUserApplication.class, args);
+	}
+
+}
---
+++ slendit-user/src/test/java/com/fq/slendit/user/SlenditUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fq.slendit.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlenditUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
