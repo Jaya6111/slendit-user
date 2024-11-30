4528ee923238c468948f10521d606d6c6e5aa516
jaya surya <jaya6111@gmail.com>
2024-11-30 17:15:36 +0530

    Testing the application

--- slendit-user/src/main/java/com/fq/slendit/user/SlenditUserApplication.java
+++ slendit-user/src/main/java/com/fq/slendit/user/SlenditUserApplication.java
@@ -8,6 +8,7 @@ public class SlenditUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SlenditUserApplication.class, args);
+		System.out.println("Hello user application");
 	}
 
 }
